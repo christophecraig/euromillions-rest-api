379f08dcae5fdc3a65845915fabead85fb2afd29
Christophe Craig <christophe.craig@viacesi.fr>
2018-12-21 11:14:39 +0100

    enabled cors

--- main.go
+++ main.go
@@ -26,6 +26,12 @@ type Result struct {
 
 var results []Result
 
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+    (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 // fun main()
 func main() {
 
@@ -72,9 +78,11 @@ func main() {
 }
 
 func GetResults(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
 	json.NewEncoder(w).Encode(results)
 }
 func GetResult(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
 	params := mux.Vars(r)
 	for _, item := range results {
 		// fmt.Sprintf pour passer l'int en string
