ea5bff5a831dd42808f41bcb3fd33fb549b927bf
Christophe Craig <christophe.craig@viacesi.fr>
2019-01-08 09:37:56 +0100

    forgot ; corrected now

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,7 @@
 wget -q https://www.fdj.fr/generated/game/euromillions/euromillions_4.zip -O ./euromillions.zip;
-echo "downloaded from www.fdj.fr"
+echo "downloaded from www.fdj.fr";
 unzip -o ./euromillions.zip;
-echo "extracted content to euromillions_4.csv"
+echo "extracted content to euromillions_4.csv";
 /usr/bin/php $PWD/php-import/import_euromillions.php;
 echo "imported csv into table results\r";
 echo "building main.go\r";
