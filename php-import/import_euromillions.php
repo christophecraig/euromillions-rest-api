1c8817560179ff1b64fe0f70e394e03af58e4460
Christophe Craig <christophe.craig@viacesi.fr>
2019-01-08 09:27:20 +0100

    added comments for command line

--- php-import/import_euromillions.php
+++ php-import/import_euromillions.php
@@ -4,6 +4,7 @@ $db = new PDO('mysql:dbname=euromillions;host=127.0.0.1', 'root', 'root');
 $file = fopen('./euromillions_4.csv', 'r');
 $csv = fgetcsv($file, 0, ';');
 $db->exec('truncate table results');
+echo "truncated table results\n";
 while ($row = fgetcsv($file, 0, ';')) {
   $query = 'insert into results (b1, b2, b3, b4, b5, e1, e2, myMillion, date, weekday) values ('.
     $row[5].',
